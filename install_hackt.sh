f30cececb665788b0e461b80759784375d3cc80e
Ben Varkey Benjamin <ben@kanthari.mac>
2018-01-15 18:45:01 -0800

    Minor changes to info messages in install script

--- install_hackt.sh
+++ install_hackt.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo "[INFO] Use 'bash' instead of 'sh'"
+echo "[DOCKER:INFO] Using 'bash' instead of 'sh'"
 rm /bin/sh
 ln -s /bin/bash /bin/sh
 
-echo "Install HACKT dependencies"
+echo "[DOCKER:INFO] Installing HACKT dependencies"
 cd /hackt/deps
 tar -xjf bison-2.3.tar.bz2
 tar -xjf flex-2.5.35.tar.bz2
 
-echo "Install bison-2.3"
+echo "[DOCKER:INFO] Installing bison-2.3"
 cd ./bison-2.3
 ./configure --prefix=/usr/local
 make
 make install
 
-echo "Install flex-2.5.34"
+echo "[DOCKER:INFO] Installing flex-2.5.34"
 cd ../flex-2.5.35
 ./configure --prefix=/usr/local
 make
 make install
 
-echo "Install HACKT"
+echo "[DOCKER:INFO] Installing HACKT"
 cd /hackt/src
 ./bootstrap
 sed -i.orig -e '/link_all_deplibs/s|=no|=yes|' config/libtool.m4
@@ -29,4 +29,4 @@ CC=/usr/bin/gcc-4.7 CXX=/usr/bin/g++-4.7 CFLAGS="-O2 -Wno-error" CXXFLAGS="-O2 -
 make
 make install
 
-echo "Installation done"
+echo "[DOCKER:INFO] Installation done"
